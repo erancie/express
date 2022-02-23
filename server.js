83c38cb6ee561c941a9db9f2867aa27d21760705
Elliot Rancie <erparker00@gmail.com>
2022-02-23 18:03:10 +1100

    routes and controller schaff

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const { send } = require('process');
 const cors = require("cors");
 require('dotenv').config()
 const path = require('path');
+const passport = require('passport');
+
+const users = require('./routes/users');
+
 
 //Test*************** ////////
 
@@ -29,10 +33,11 @@ let app = express();
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(cors())
 app.use(bodyParser.json())
+
 const base= `${__dirname}/public`;
 
-
-//Static declaration - point to built files in React client
+// - Static declaration -
+//Redirect these paths and point to built files in React client
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/login', express.static(path.join(__dirname, 'client/build')));
 app.use('/login2', express.static(path.join(__dirname, 'client/build')));
@@ -42,11 +47,13 @@ app.use('/newtask', express.static(path.join(__dirname, 'client/build')));
 app.use('/findtask/:id', express.static(path.join(__dirname, 'client/build')));
 app.use('/findtask', express.static(path.join(__dirname, 'client/build')));
 
+     
 
 ///////////////////////// ***API*** ////////////////////////////////////
 ///////////////////  //find user according to email 
 // LOGIN //////////  //if exists, compare form password to user password 
 app.post('/login', (req, res)=> { 
+  console.log(this)
   const {email, password} = req.body;
   User.findOne({ email: email}, (e, user)=>{
     if (e) { res.send("No such email.") }    //*FIX*- handle no email err
@@ -69,85 +76,9 @@ app.post('/login', (req, res)=> {
 
 
 ///// USERS API //////////////////////////////
-app.get('/users', (req,res)=>{
-  User.find({}, (err, users)=>{
-    if(err) send(err)
-    else res.send(users)
-  })
-})
+//for all http requests to /users path use() the users router
+app.use('/users', users)
 
-//- USER Registration //
-app.post('/users', (req, res)=> { //change to /users
-  //create new user from body fields
-  const newUser = new User({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    country: req.body.country,
-    password: req.body.password,
-    password2: req.body.password2,
-    address: req.body.address,
-    address2: req.body.address2,
-    city: req.body.city,
-    state: req.body.state,
-    phoneNumber: req.body.phoneNumber,
-    zip: req.body.zip,
-    terms: req.body.terms
-  });
-  //save new user to db
-  newUser.save((err)=>{
-    err ? console.log(err) : console.log('New User Inserted Succesfully')
-  })
-      //MAILCHIMP////////
-      //get body form fields for mailchimp
-      let {firstname, lastname, email} = req.body;
-      console.log(firstname, lastname, email);
-      //mailchimp api key
-      const apiKey = process.env.MAILCHIMP_KEY;
-      //mailchimp audience/list id
-      const listId = process.env.LIST_ID;
-      //url to your list
-      const url = `https://us5.api.mailchimp.com/3.0/lists/${listId}`
-      const options = {
-        method: "POST",
-        auth: `mystring:${apiKey}`
-      } 
-      //create request to mailchimp w/ https
-      const request = https.request(url, options, (res)=>{
-        res.on('data', (data)=>{
-          console.log(JSON.parse(data))
-        })
-      })
-      //create request mailchimp obj with body fields
-      const data = {
-        members:[
-          {
-            email_address: email,
-            status: 'subscribed',
-            merge_fields:{
-              FNAME: firstname,
-              LNAME: lastname }}]}
-      jsonData = JSON.stringify(data);  //convert to JSON format
-      // request.write(jsonData)   // enable API Key & uncomment to enable mailchimp
-      // request.end()
-      //END MAILCHIMP----
-
-  //redirect to home/welcome page - or dashboard. 
-  if(res.statusCode === 200){
-    res.json({success:true})
-  }else{   //front end error //**TO FIX** - only sends status 200
-    res.redirect('/404.html')
-  }
-})//end post /users
-
-//delete all users
-app.delete('/users', (req, res)=>{
-  User.deleteMany((err)=>{
-    if (err) res.send(err)
-    else res.send('Deleted all users.')
-  })
-})
-//END USERS---------------------------------------------
 
 //////////// EXPERTS API //////// TASK 6.1P /////////////////// 
 app.route('/experts')
@@ -272,8 +203,9 @@ app.route('/tasks')
   .catch((err)=> console.log(err))
   res.json((`task saved to db: ${newTask}`))
 })
- //get task item
+
 app.route('/tasks/:id')
+//get single task
 .get((req, res)=> { 
   console.log(req.params.id)
   Task.find({_id: req.params.id}, (err, task)=>{
@@ -322,3 +254,13 @@ app.listen(port, (req, res)=>{
   console.log(`Server is running on port: ${port}`);
   console.log('process.env.PORT: ' + process.env.PORT)
 }) 
+
+
+
+//create a local strategy for passport
+
+  //verify username and password
+
+    //get record from db
+
+    //compare password using bcrypt
